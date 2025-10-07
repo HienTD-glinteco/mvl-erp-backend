96d3e8f770451ca38326ef1cabab188efacaddb6
HienTD-glinteco <112367354+HienTD-glinteco@users.noreply.github.com>
2025-10-07 10:47:34 +0700

    Add a dedicated logger for our audit trails (#111)

--- settings/base/logging.py
+++ settings/base/logging.py
@@ -1,8 +1,13 @@
+import os
+from typing import Any
+
+from .audit_logging import AUDIT_LOG_DISABLED
 from .base import BASE_DIR, LOG_LEVEL
 
-LOG_DIRECTORY = BASE_DIR
+LOG_DIRECTORY = os.path.join(BASE_DIR, "logs")
 
-LOGGING = {
+
+LOGGING: dict[str, Any] = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
@@ -25,3 +30,30 @@ LOGGING = {
         "level": LOG_LEVEL,
     },
 }
+
+if not AUDIT_LOG_DISABLED:
+    if not os.path.exists(LOG_DIRECTORY):
+        os.makedirs(LOG_DIRECTORY)
+    if not os.path.exists(os.path.join(LOG_DIRECTORY, "audit_logging")):
+        os.makedirs(os.path.join(LOG_DIRECTORY, "audit_logging"))
+
+    LOGGING["formatters"]["audit_logging"] = {
+        "format": "{levelname} {asctime} {module} {message}",
+        "style": "{",
+    }
+    LOGGING["handlers"]["audit_file"] = {
+        "level": "INFO",
+        "class": "logging.handlers.TimedRotatingFileHandler",
+        "filename": os.path.join(LOG_DIRECTORY, "audit_logging", "audit.log"),
+        "when": "midnight",
+        "backupCount": 30,
+        "utc": True,
+        "formatter": "audit_logging",
+    }
+
+    LOGGING.setdefault("loggers", {})
+    LOGGING["loggers"]["audit_logging"] = {  # A dedicated logger for our audit trails
+        "handlers": ["audit_file"],
+        "level": "INFO",
+        "propagate": False,
+    }
