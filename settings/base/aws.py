3d6b013eda1bd29b524bc7e3e11ff5ba81d96864
Hien Tran Doan <hien.trandoan@glinteco.com>
2025-11-21 09:48:10 +0700

    Add dbbackup

--- settings/base/aws.py
+++ settings/base/aws.py
@@ -4,6 +4,7 @@ from .base import config
 AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID", default="")
 AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", default="")
 AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME", default="")
+AWS_DB_STORAGE_BUCKET_NAME = config("AWS_DB_STORAGE_BUCKET_NAME", default="")
 AWS_REGION_NAME = config("AWS_REGION_NAME", default="")
 
 # S3 settings
