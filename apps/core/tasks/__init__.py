211ab6fe1c4f135303d4b84d29fc9a6ae2cfd0d1
Trang Nguyen <trang.nguyenngoc@glinteco.com>
2025-12-19 13:00:59 +0700

    fix: device otp send mail error

--- apps/core/tasks/__init__.py
+++ apps/core/tasks/__init__.py
@@ -1,9 +1,11 @@
 from .email import (
+    send_otp_device_change_task,
     send_otp_email_task,
     send_password_reset_email_task,
 )
 
 __all__ = [
+    "send_otp_device_change_task",
     "send_otp_email_task",
     "send_password_reset_email_task",
 ]
